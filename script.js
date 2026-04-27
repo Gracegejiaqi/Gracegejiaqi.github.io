791f12c73b730914d962c80913f82fbf2d8d8009
Gracegejiaqi <147311433+Gracegejiaqi@users.noreply.github.com>
2026-04-27 21:12:16 +0800

    Add dark animated homepage

---
+++ script.js
@@ -0,0 +1,6 @@
+const root = document.documentElement;
+
+window.addEventListener("pointermove", (event) => {
+  root.style.setProperty("--mouse-x", `${event.clientX}px`);
+  root.style.setProperty("--mouse-y", `${event.clientY}px`);
+});
